6605e11ca1599882de87f3954b679754ace9831f
Robin Bertin <bertinr@ensipc167.ensimag.fr>
2019-10-07 16:34:47 +0200

    Début de structure du code de affichage et niveau. Recherche de comment afficher en découvrant petit à petit

--- src/affichage.py
+++ src/affichage.py
@@ -1,2 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import niveau
+
+class Jeu:
+    """
+    Classe gérant le jeu dans sa généralité
+    """
+    def __init__(self):
+        """
+        Initialise le plateau de jeu en générant des salles/couloirs aléatoirement
+        et un personnage en (0,0)
+        """
+        self.perso = [0,0]
+        self.plateau = np.zeros((20,30))
+
+    def refresh(self):
+        """
+        Affiche le plateau de jeu
+        """
+        pass
--- src/niveau.py
+++ src/niveau.py
@@ -1,2 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+Module gérant la génération et les opérations sur les
+niveaux du jeu
+"""
+
+class Salle:
+    """
+    essai
+    """
+    def __init__(self):
+        self.deja = 0
+        self.pos = [1,1]
+        self.taille = [2,3]
+
+
+    def introduit(self, mat):
+        pass
+
+
+def generation(jeu):
+    salles = []  # A generer aléatoirement
+    for salle in salles:
+        salle.introduit(jeu.plateau)
